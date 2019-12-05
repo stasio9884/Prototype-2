819ea925dd2fcbbbf9cf6732465ce275bc6c99f9
courtney stasio <56166375+stasio9884@users.noreply.github.com>
2019-12-05 08:30:22 -0800

    made it so the person does not continue off the left side

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
+    public float speed = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-     horizontalInput = Input.GetAxis("Horizontal");
+        if (transform.position.x < -10)
+        {
+            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
 }
