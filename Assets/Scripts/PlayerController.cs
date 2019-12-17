589bee5da988ff4b183fa134474def3679917d23
courtney stasio <56166375+stasio9884@users.noreply.github.com>
2019-12-17 08:49:07 -0800

    working on animals and food

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10.0f;
+    public GameObject projectilePrefab; 
     // Start is called before the first frame update
     void Start()
     {
@@ -25,6 +26,11 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(xRange , transform.position.y, transform.position.z);
         }
+      if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position,projectilePrefab.transform.rotation);
+        }
+
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
